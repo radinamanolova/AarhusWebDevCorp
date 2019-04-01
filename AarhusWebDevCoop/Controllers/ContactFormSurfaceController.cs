2f02558927feca9cbb96dfdc269c7ee5fd412051
Radina Manolova <radinamanolova@users.noreply.github.com>
2019-04-01 12:58:49 +0200

    initial commit

---
+++ AarhusWebDevCoop/Controllers/ContactFormSurfaceController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+using AarhusWebDevCoop.ViewModels;
+using System.Net.Mail;
+using Umbraco.Core.Models;
+
+namespace AarhusWebDevCoop.Controllers
+{
+    public class ContactFormSurfaceController : SurfaceController
+    {
+        // GET: ContactFormSurface
+        public ActionResult Index()
+        {
+            return PartialView("ContactForm", new ContactForm());
+        }
+
+        [HttpPost]
+        public ActionResult HandleFormSubmit(ContactForm model)
+        {
+            if (!ModelState.IsValid) { return CurrentUmbracoPage(); }
+            TempData["success"] = true;
+
+            MailMessage message = new MailMessage();
+            message.To.Add("username@eaaa.dk");
+            message.Subject = model.Subject;
+            message.From = new MailAddress(model.Email, model.Name);
+            message.Body = model.Message;
+
+            IContent msg = Services.ContentService.CreateContent(model.Subject, CurrentPage.Id, "message");
+            msg.SetValue("messageName", model.Name);
+            msg.SetValue("email", model.Email);
+            msg.SetValue("subject", model.Subject);
+            msg.SetValue("messageContent", model.Message);
+
+            Services.ContentService.Save(msg);
+
+            using (SmtpClient smtp = new SmtpClient())
+            {
+                smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                smtp.UseDefaultCredentials = false;
+                smtp.EnableSsl = true;
+                smtp.Host = "smtp.gmail.com";
+                smtp.Port = 587;
+                smtp.Credentials = new System.Net.NetworkCredential("radina.manolova96@gmail.com", "lcgh zbss hdfj cvdy");
+                // send mail
+                smtp.Send(message);
+            }
+
+            return RedirectToCurrentUmbracoPage();
+        }
+    }
+}
+    
---
+++ AarhusWebDevCoop/ViewModels/ContactForm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace AarhusWebDevCoop.ViewModels
+{
+    public class ContactForm
+    {
+        
+            [Required(ErrorMessage = "Please enter your name")]
+            public string Name { get; set; }
+
+            [Required(ErrorMessage = "Please enter your email")]
+            [Display(Name = "Email")]
+            [RegularExpression(@"^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$",
+            ErrorMessage = "Please enter a correct email address")]
+            public string Email { get; set; }
+
+            [Required(ErrorMessage = "Please enter a subject")]
+            public string Subject { get; set; }
+
+            [Required(ErrorMessage = "Please enter a message")]
+            public string Message { get; set; }
+        
+    }
+}
